0dcb1fa415c1eb158cb5841ecef08e4eecefd917
phonil <phonil@naver.com>
2023-12-18 01:00:37 +0900

    feat: 친구 목록 API

--- src/pages/Friends.js
+++ src/pages/Friends.js
@@ -1,22 +1,65 @@
 import styled from 'styled-components';
 import { ReactComponent as Paper } from '../imgs/Paper.svg';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
 
-const Friends = () => (
+const Friends = () => {
+  const [searchData, setSearchData] = useState([]);
+  const [searchWord, setSearchWord] = useState(""); // 검색어 상태 추가
+
+  // 친구 목록
+  const [friendList, setFriendList] = ustState([]);
+
+  const search = () => {
+    const userId = 1;
+
+    axios.get(`/users/search/${searchWord}`)
+      .then((response) => {
+        setSearchData(response.data);
+        console.log(response.data);
+      })
+      .catch(error => console.error(error));
+  };
+
+  const handleInputChange = (event) => {
+    setSearchWord(event.target.value); // 입력 값으로 searchWord 상태 업데이트
+  };
+
+
+  const findFriendList = () => {
+    axios.get(`/friends/${userId}`)
+      .then((response) => {
+        setFriendList(response.data.Friends)
+        console.log(response.data.Friends)
+      })
+      .catch(error => console.error(error));
+
+  }
+
+  return (
   <Wrapper>
     <LeftBox>
       <BackBtn to="/home">내 우주로 돌아가기</BackBtn>
       <PaperBox>
         <ContentBox>
-          <Title>친구 검색 </Title>
+          <Title>친구 검색</Title>
           <SearchBox>
-            <SearchInput placeholder="이메일을 검색하세요" />
-            <SearchBtn>검색 </SearchBtn>
+            <SearchInput 
+              placeholder="이메일을 검색하세요" 
+              value={searchWord} // input의 value를 searchWord 상태와 연결
+              onChange={handleInputChange} // 입력 시 handleInputChange 호출
+            />
+            <SearchBtn onClick={search}> 검색 </SearchBtn>
           </SearchBox>
-          <List>
-            <p>이유라</p>
-            <FriendBtn>친구 신청</FriendBtn>
-          </List>
+          <ResultBox>
+            {searchData.map((user, index) => (
+              <List key={user.email}>
+                <p>{user.nickname}</p>
+                <FriendBtn>친구 신청</FriendBtn>
+              </List>
+            ))}
+          </ResultBox>
         </ContentBox>
       </PaperBox>
     </LeftBox>
@@ -24,18 +67,21 @@ const Friends = () => (
       <PaperBox right>
         <ContentBox right>
           <Title>친구 관리</Title>
-          <List>
-            <FriendName>이유라</FriendName>
-            <BtnBox>
-              <FriendBtn>방문</FriendBtn>
-              <FriendBtn>삭제</FriendBtn>
-            </BtnBox>
-          </List>
+            {friendList.map((user, index) => (
+              <List key={user.id}>
+              <p>{user.name}</p>
+              <BtnBox>
+                <FriendBtn>방문</FriendBtn>
+                <FriendBtn>삭제</FriendBtn>
+              </BtnBox>
+              </List>
+            ))}
+            
         </ContentBox>
       </PaperBox>
     </RightBox>
   </Wrapper>
-);
+)};
 
 const Wrapper = styled.div`
   width: 100%;
@@ -150,7 +196,12 @@ const SearchBtn = styled.p`
 `;
 
 const ResultBox = styled.div`
-  width: 312px;
+  width: 420px;
+  height: 400px;
+  margin-top: 10px;
+  margin-left: 20px;
+  padding-left: 30px;
+  overflow: auto;
 `;
 const Title = styled.p`
   text-align: center;
